6165260989852630b2583ff5568ac65056ed9c5f
llyq <lyq_2050@163.com>
2020-07-03 23:31:35 +0800

    IO密集型：多线程 CPU密集型：单线程

--- learndata/concurrency_and_performance.py
+++ learndata/concurrency_and_performance.py
@@ -78,50 +78,50 @@ import requests
 
 # 多线程 全局变量的问题  多线程使用全局变量会造成安全问题
 
-# 全局变量
-a = 100
-b = [100]
-
-def func1():
-    global a
-    for i in range(1000000):
-        metaA.acquire()
-        metaB.acquire()
-        print('------------1----------------')
-        a += 1    # 暂停了，切换到了任务2
-        # b[0] += 1
-        metaB.release()
-        metaA.release()
-    print('func1--线程1修改完a:', a)
-
-
-def func2():
-    global a
-    for i in range(1000000):
-        metaB.acquire()  # 上锁
-        metaA.acquire()  # 上锁
-        print('-------------2-------------')
-        a += 1    # a = 10000, 暂停了，切换到了任务1
-        # b[0] += 1
-        metaA.release()  # 师傅锁
-        metaB.release()  # 释放锁
-    print('func2--线程2修改完a:', a)
-
-# 创建锁
-metaA = threading.Lock()
-metaB = threading.Lock()
-
-t1 = threading.Thread(target=func1)
-t2 = threading.Thread(target=func2)
-
-t1.start()
-t2.start()
-
-t1.join()
-t2.join()
-
+# 互斥锁
+# a = 100
+# b = [100]
+#
+# def func1():
+#     global a
+#     for i in range(1000000):
+#         metaA.acquire()
+#         metaB.acquire()
+#         print('------------1----------------')
+#         a += 1    # 暂停了，切换到了任务2
+#         # b[0] += 1
+#         metaB.release()
+#         metaA.release()
+#     print('func1--线程1修改完a:', a)
+#
+#
+# def func2():
+#     global a
+#     for i in range(1000000):
+#         metaB.acquire()  # 上锁
+#         metaA.acquire()  # 上锁
+#         print('-------------2-------------')
+#         a += 1    # a = 10000, 暂停了，切换到了任务1
+#         # b[0] += 1
+#         metaA.release()  # 师傅锁
+#         metaB.release()  # 释放锁
+#     print('func2--线程2修改完a:', a)
+#
+# # 创建锁
+# metaA = threading.Lock()
+# metaB = threading.Lock()
+#
+# t1 = threading.Thread(target=func1)
+# t2 = threading.Thread(target=func2)
+#
+# t1.start()
+# t2.start()
+#
+# t1.join()
+# t2.join()
+#
+# # print(a)
 # print(a)
-print(a)
 
 # 作业：10个线程对象，每个线程发送100次请求，计算平均所需耗时
 # count = 0
@@ -155,6 +155,63 @@ print(a)
 #     end_time = time.time()
 #     print('平均耗时：', (end_time-start_time)/count)
 #     print(count)
+
+# GIL IO密集型、CPU密集型
+a = 100
+
+
+def func1():
+    global a
+    for i in range(100000000):
+        a += 1    # 暂停了，切换到了任务2
+    print('func1--线程1修改完a:', a)
+
+
+def func2():
+    global a
+    for i in range(100000000):
+        a += 1    # a = 10000, 暂停了，切换到了任务1
+    print('func2--线程2修改完a:', a)
+
+
+def func3():
+    for i in range(50):
+        requests.get('http://www.baidu.com')
+    print('func3--线程3修改完a:', a)
+
+
+def func4():
+    for i in range(50):
+        requests.get('http://www.baidu.com')
+    print('func4--线程4修改完a:', a)
+
+# CPU密集型多线程 14.351438522338867  比单线程慢，需要不同线程间切换
+# start_time = time.time()
+# t1 = threading.Thread(target=func3) # 网络IO密集型 4.18
+# t2 = threading.Thread(target=func4)
+# t1.start()
+# t2.start()
+# t1.join()
+# t2.join()
+# end_time = time.time()
+# print(a)
+# print(end_time-start_time)
+
+# 单线程  11.969712257385254
+start_time = time.time()
+func3() # IO密集型 8.085339069366455
+func4()
+end_time = time.time()
+print(end_time-start_time)
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
